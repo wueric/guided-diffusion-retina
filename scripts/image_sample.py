9d635ff7ea85471a99b0585405c4952f9e3fea56
Eric Gene Wu <wueric@peggyo.stanford.edu>
2023-11-08 16:24:29 -0800

    sampling mods worked

--- scripts/image_sample.py
+++ scripts/image_sample.py
@@ -8,7 +8,7 @@ import os
 
 import numpy as np
 import torch as th
-import torch.distributed as dist
+#import torch.distributed as dist
 
 #from guided_diffusion import dist_util, logger
 from guided_diffusion import logger
@@ -37,7 +37,7 @@ def main():
     #    dist_util.load_state_dict(args.model_path, map_location="cpu")
     #)
     model.load_state_dict(
-        args.model_path, map_location="cpu"
+        th.load(args.model_path, map_location="cpu")
     )
 
     #model.to(dist_util.dev())
@@ -54,7 +54,8 @@ def main():
         model_kwargs = {}
         if args.class_cond:
             classes = th.randint(
-                low=0, high=NUM_CLASSES, size=(args.batch_size,), device=dist_util.dev()
+                #low=0, high=NUM_CLASSES, size=(args.batch_size,), device=dist_util.dev()
+                low=0, high=NUM_CLASSES, size=(args.batch_size,), device=device
             )
             model_kwargs["y"] = classes
         sample_fn = (
@@ -70,15 +71,17 @@ def main():
         sample = sample.permute(0, 2, 3, 1)
         sample = sample.contiguous()
 
-        gathered_samples = [th.zeros_like(sample) for _ in range(dist.get_world_size())]
-        dist.all_gather(gathered_samples, sample)  # gather not supported with NCCL
-        all_images.extend([sample.cpu().numpy() for sample in gathered_samples])
+        #gathered_samples = [th.zeros_like(sample) for _ in range(dist.get_world_size())]
+        #dist.all_gather(gathered_samples, sample)  # gather not supported with NCCL
+        #all_images.extend([sample.cpu().numpy() for sample in gathered_samples])
+        all_images.append(sample.cpu().numpy())
         if args.class_cond:
-            gathered_labels = [
-                th.zeros_like(classes) for _ in range(dist.get_world_size())
-            ]
-            dist.all_gather(gathered_labels, classes)
-            all_labels.extend([labels.cpu().numpy() for labels in gathered_labels])
+            #gathered_labels = [
+            #    th.zeros_like(classes) for _ in range(dist.get_world_size())
+            #]
+            #dist.all_gather(gathered_labels, classes)
+            #all_labels.extend([labels.cpu().numpy() for labels in gathered_labels])
+            all_labels.append(labels.cpu().numpy())
         logger.log(f"created {len(all_images) * args.batch_size} samples")
 
     arr = np.concatenate(all_images, axis=0)
@@ -86,16 +89,16 @@ def main():
     if args.class_cond:
         label_arr = np.concatenate(all_labels, axis=0)
         label_arr = label_arr[: args.num_samples]
-    if dist.get_rank() == 0:
-        shape_str = "x".join([str(x) for x in arr.shape])
-        out_path = os.path.join(logger.get_dir(), f"samples_{shape_str}.npz")
-        logger.log(f"saving to {out_path}")
-        if args.class_cond:
-            np.savez(out_path, arr, label_arr)
-        else:
-            np.savez(out_path, arr)
 
-    dist.barrier()
+    shape_str = "x".join([str(x) for x in arr.shape])
+    out_path = os.path.join(logger.get_dir(), f"samples_{shape_str}.npz")
+    logger.log(f"saving to {out_path}")
+    if args.class_cond:
+        np.savez(out_path, arr, label_arr)
+    else:
+        np.savez(out_path, arr)
+
+    #dist.barrier()
     logger.log("sampling complete")
 
 
